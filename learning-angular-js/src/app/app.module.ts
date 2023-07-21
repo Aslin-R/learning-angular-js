5ff77f526a8da7b0175645f2977a09638573a045
aslin-r <aslinjoelr123@gmail.com>
2023-07-21 13:07:12 +0530

    tried the routing and updated the login form as add blog form

--- learning-angular-js/src/app/app.module.ts
+++ learning-angular-js/src/app/app.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
+import { Routes,RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NewcomponentComponent } from './newcomponent/newcomponent.component';
 import { BlogformComponent } from './blogform/blogform.component';
+
+const routes:Routes=[
+  {path:'blogs',component:NewcomponentComponent},
+  {path:'addBlog',component:BlogformComponent},
+  {path:'languages',component:NewcomponentComponent}
+  
+]
 
 @NgModule({
   declarations: [
@@ -15,6 +23,7 @@ import { BlogformComponent } from './blogform/blogform.component';
   imports: [
     BrowserModule,
     FormsModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
